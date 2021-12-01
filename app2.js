cba542c4c0fb3e769e7cc5dd4e7c29a545b1fb66
Christina R <xsnachannell@gmail.com>
2021-11-30 21:02:18 -0800

    added for and while loops to site

--- app2.js
+++ app2.js
@@ -3,14 +3,11 @@ var willJoin = confirm("Would you like to join our scripture of the day emailing
 
 if (willJoin == true) {
     var emailAddress = prompt("Please Enter your email address");
-    document.write("<h2>" + "Thank you for joining our mailing list!" + "</h2>");
+    document.write("<h3>" + "Thank you for joining our mailing list!" + "</h3>");
 }
 else {
     document.write("Thank you for coming to our site!");
 }
 return emailAddress;
 }
-
 let userID = confirm ("Would you like to use " + emailAddress + " as your site user name?");
-
-
--- app3.js
+++ app3.js
@@ -1,11 +1,25 @@
 function getFaveColor(){
 let mainstyle = "black";
 var favoriteColor = prompt("Please Input your favorite color");
-if (favoriteColor == mainstyle){
-    document.write("You've chosen the right color!");
+while (favoriteColor !== mainstyle) {
+favoriteColor = prompt("You've chosen the wrong color. Please try again. Hint: all the colors combined");
 }
-else {
-    favoriteColor = prompt("Please enter the right color");
-    getFaveColor();
+// if (favoriteColor == mainstyle){
+//     document.write("You've chosen the right color!");
+// }
+// else {
+//     favoriteColor = prompt("Please enter the right color");
+//     getFaveColor();
+// }
 }
+
+function showPrayerHands(){
+    let handNumber = prompt("How many prayers do you need today?");
+    let number = 0;
+    for (let i = 0; i < handNumber; i++) {
+    let url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcZjOPZougl-vMGY5s2MjeOa1ee701FYzXIkih5mfBN5JfY77aq02kxODiDtsNjvoY7X8&usqp=CAU";
+    number = number + 1;
+    document.write('<h4><img id="picture" src="' + url +'">' + " " + number + '</h4>');
+    }
 }
+    
